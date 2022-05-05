e5f02385daad1964127243fecc49ce773abcfcd7
DavideIsaia <davide.enjoy@hotmail.it>
2022-05-05 13:15:43 +0200

    messaggio e immagine aggiunti

--- js/script.js
+++ js/script.js
@@ -1,9 +1,15 @@
+// Descrizione:
+// Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+// Bonus:
+// Aggiungere alla pagina un’immagine, presa anch’essa da un data.
+
 Vue.config.devtools = true;
 const app = new Vue (
     {
         el : '#root',
         data: {
-            
+            messageText: 'Hello World!',
+            imgRandom: 'https://picsum.photos/600/300'
         }
     }
 )
